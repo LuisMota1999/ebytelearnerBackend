4a6f67ebe8329678d6eaa951c9b290dbb0519b15
LuisMota1999 <38186@ufp.edu.pt>
2024-03-08 17:51:52 +0000

    feature: Required a teacher to be displayed in course now

--- ebyteLearner/DTOs/Course/CreateCourseRequestDTO.cs
+++ ebyteLearner/DTOs/Course/CreateCourseRequestDTO.cs
@@ -7,7 +7,10 @@ namespace ebyteLearner.DTOs.Course
         [Required]
         public string CourseName { get; set; }
         public string CourseDescription { get; set; }
+        [Required]
         public float CoursePrice { get; set; }
+        [Required]
+        public Guid CourseTeacherID { get; set; }
         public DateTimeOffset CreatedDate { get; init; } = DateTimeOffset.Now;
     }
 }
--- ebyteLearner/obj/Release/net7.0/ebyteLearner.AssemblyInfo.cs
+++ ebyteLearner/obj/Release/net7.0/ebyteLearner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ebyteLearner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8d83cfd528cf5c926020e9cad58f8f25c605ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ebyteLearner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ebyteLearner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
